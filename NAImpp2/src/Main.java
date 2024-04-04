575fa76d44d3a11f6fefa30bc25ab820be28f5b0
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-04 13:58:28 +0200

    add method to choose train set

--- NAImpp2/src/Main.java
+++ NAImpp2/src/Main.java
@@ -1,5 +1,9 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        File trainSet = userInterface.chooseTrainSet();
 
     }
 }
--- NAImpp2/src/UserInterface.java
+++ NAImpp2/src/UserInterface.java
@@ -1,2 +1,30 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class UserInterface {
+    Scanner scanner;
+
+
+    private File chooseFile() {
+        scanner = new Scanner(System.in);
+        String path = scanner.nextLine();
+        File result = new File(path);
+        if(result.isFile()){
+            if(path.substring(path.lastIndexOf('.') + 1).equals("csv")){
+                return result;
+            } else {
+                System.out.println("You should choose .csv file, try again.");
+                return chooseFile();
+            }
+
+        } else {
+            System.out.println("No such file, try again.");
+            return chooseFile();
+        }
+    }
+
+    public File chooseTrainSet() {
+        System.out.println("Input path to file with Train Set:");
+        return chooseFile();
+    }
 }
