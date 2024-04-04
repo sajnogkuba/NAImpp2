1fac52317f8b59dd34eaab57d39d7ebc8ec51c74
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-04 14:36:02 +0200

    add methods to generate wights vector and train Set List

--- NAImpp2/src/Perceptron.java
+++ NAImpp2/src/Perceptron.java
@@ -1,16 +1,41 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Perceptron {
     private int numberOfValuesInVector;
+    private List<List<String>> trainSet;
+    private List<Double> weights;
     public Perceptron(File trainSet) {
         try (Scanner scanner = new Scanner(trainSet);){
-            numberOfValuesInVector = (int)Arrays.stream(scanner.nextLine().split(",")).count() - 1;
-            System.out.println(numberOfValuesInVector);
+            numberOfValuesInVector = (int)Arrays.stream(scanner.nextLine().split(",")).count();
+            this.trainSet = generateTrainSetAsList(trainSet);
+            this.weights = generateWeightsVector();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        System.out.println(this.weights);
+    }
+
+    private List<Double> generateWeightsVector() {
+        Random random = new Random();
+        List<Double> result = new ArrayList<>();
+        for (int i = 0; i < numberOfValuesInVector; i++) {
+            result.add(random.nextDouble());
+        }
+        return result;
+    }
+
+    private List<List<String>> generateTrainSetAsList(File trainSet){
+        List<List<String>> result = new ArrayList<>();
+        try (Scanner scanner = new Scanner(trainSet);){
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                result.add(Arrays.stream(line.split(",")).toList());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
