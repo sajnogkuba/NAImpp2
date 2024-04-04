60addcabc17a2ecace5f5dad004c8c083f2b3bfd
Jakub Sajnóg <jakubsajnog@gmail.com>
2024-04-04 14:02:57 +0200

    add method to choose program mode

--- NAImpp2/src/Main.java
+++ NAImpp2/src/Main.java
@@ -1,9 +1,11 @@
 import java.io.File;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         File trainSet = userInterface.chooseTrainSet();
+        int programMode = userInterface.chooseMode();
 
     }
 }
--- NAImpp2/src/UserInterface.java
+++ NAImpp2/src/UserInterface.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -26,5 +27,28 @@ public class UserInterface {
     public File chooseTrainSet() {
         System.out.println("Input path to file with Train Set:");
         return chooseFile();
+    }
+
+    public int chooseMode() {
+        System.out.println("""
+                Choose option:\s
+                \t1. Choose file with test set.\s
+                \t2. Input vectors with data in interface.""");
+        return selectOption(2);
+    }
+
+    public int selectOption(int max) {
+        scanner = new Scanner(System.in);
+        try{
+            int input = scanner.nextInt();
+            if(input < 1 | input > max){
+                System.out.println("You can choose options from 1 to " + max + " try again.");
+                return selectOption(max);
+            }
+            return input;
+        } catch (InputMismatchException e){
+            System.out.println("You should type in an integer, try again.");
+            return selectOption(max);
+        }
     }
 }
